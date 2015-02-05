3cfe6d7ea66b6dd9ebb549a0a89db0420f35f8b9
Matt Bentley <mbentley@mbentley.net>
2015-02-05 16:29:09 -0500

    updated swarm to use the official swarm RC build

---
+++ scripts/get-swarm-manager.rb
@@ -0,0 +1,17 @@
+#!/opt/vagrant/embedded/bin/ruby
+
+require 'YAML'
+
+SPEC_FILE=ENV['STEVEDORE_FILE'] || File.expand_path(File.dirname(__FILE__) + '/..') + '/stevedore.yaml'
+
+SPEC=YAML::load(File.open(SPEC_FILE))
+
+swarm_hosts= []
+
+SPEC['vms'].each do |vm|
+  vm['roles'].each do |role|
+    swarm_hosts << vm['name'] if role == 'swarm-manager'
+  end
+end
+
+puts swarm_hosts.map{|x| x + '.docker.vm:12345'}.join(',')
