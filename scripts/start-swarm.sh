9149478c27d21e0b2191ee72e7a298865e70c016
Matt Bentley <mbentley@mbentley.net>
2015-02-16 14:30:25 -0500

    fixed start script

--- scripts/start-swarm.sh
+++ scripts/start-swarm.sh
@@ -8,8 +8,8 @@ check_landrush () {
   fi
 }
 
-SWARM_HOSTS="$("$DIR/get-swarm-hosts.rb")"
-SWARM_MANAGER="$("$DIR/get-swarm-manager.rb")"
+SWARM_HOSTS="$("$DIR/get-swarm-hosts.rb -h")"
+SWARM_MANAGER="$("$DIR/get-swarm-hosts.rb -m")"
 
 IFS=',' read -ra HOSTS <<< "${SWARM_HOSTS}"
 
