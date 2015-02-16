dd83f20cbabc71b9491503491e72f7ad2ce4e740
Matt Bentley <mbentley@mbentley.net>
2015-02-16 13:56:00 -0500

    moved swarm into a container

--- scripts/start-swarm.sh
+++ scripts/start-swarm.sh
@@ -8,15 +8,19 @@ check_landrush () {
   fi
 }
 
-SWARM_HOSTS="$(${DIR}/get-swarm-hosts.rb -h)"
-SWARM_MANAGER="$(${DIR}/get-swarm-hosts.rb -m)"
+SWARM_HOSTS="$("$DIR/get-swarm-hosts.rb")"
+SWARM_MANAGER="$("$DIR/get-swarm-manager.rb")"
+
+IFS=',' read -ra HOSTS <<< "${SWARM_HOSTS}"
 
 check_landrush
 
-vagrant ssh ${SWARM_MANAGER%%.*} \
+vagrant ssh ${SWARM_MANAGER%.docker.vm:12345} \
   -c "sudo /vagrant/scripts/swarm-manager.sh ${SWARM_HOSTS}" \
   > /dev/null 2>&1
 
+SWARM_MANAGER_IP="$(dig +short -p 10053 @localhost ${SWARM_MANAGER%:12345})"
+
 echo "export DOCKER_TLS_VERIFY=yes"
-echo "export DOCKER_CERT_PATH=$(dirname ${DIR})/etc"
-echo "export DOCKER_HOST=${SWARM_MANAGER}:12345"
+echo "export DOCKER_CERT_PATH=$(dirname $DIR)/etc"
+echo "export DOCKER_HOST=${SWARM_MANAGER_IP}:12345"
