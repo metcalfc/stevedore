a97d6eebbe34e45a51de920e2e8b7e92893c0321
Matt Bentley <mbentley@mbentley.net>
2015-02-16 14:45:59 -0500

    fixed broken rekey

--- scripts/re-key.sh
+++ scripts/re-key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 re-key-host () {
-  ssh $1 "sudo rm -f /etc/docker/key.json && sudo service docker restart && sleep 1 && docker info | grep ID"
+  vagrant ssh ${1} -c "sudo rm -f /etc/docker/key.json && sudo service docker restart && sleep 1 && docker info | grep ID"
 }
 
 for i in $(vagrant status | grep running | awk {'print $1'}); do
-  re-key-host $i
+  re-key-host ${i}
 done
