35b39c687d41afd3d8d92684571b12fdb53ae047
Chad Metcalf <metcalfc@gmail.com>
2015-12-11 16:52:59 -0800

    Adding a make ucp bundle command.

--- env.sh
+++ env.sh
@@ -6,6 +6,19 @@ make-dtr-bundle () {
     cp $DIR/etc/ssl/private/dtr.docker.vm.key $DIR/dtr/key.pem
     cp $DIR/etc/ssl/certs/dtr.docker.vm.crt $DIR/dtr/cert.pem
 }
+
+make-ucp-bundle () {
+    DUCP="${1:-ducp.docker.vm}"
+    USER_NAME="${1:-admin}"
+    PASSWD="${1:-orca}"
+    TMPFILE=$(mktemp -d "/tmp/${DUCP}.XXXXXX") || exit 1
+    JSON="{\"username\":\"${USER_NAME}\",\"password\":\"${PASSWD}\"}"
+    AUTHTOKEN=$(curl -sk -d "${JSON}" "https://${DUCP}/auth/login" | jq -r .auth_token)
+    curl -k -H "X-Access-Token:admin:$AUTHTOKEN" "https://${DUCP}/api/clientbundle" -o "${TMPFILE}/bundle.zip"
+    unzip  "${TMPFILE}/bundle.zip" -d "${DIR}/ducp"
+    rm -rf "${TMPFILE}" || echo "Couldn't delete ${TMPFILE}"
+}
+
 use-dtr () {
     export DOCKER_HOST=tcp://dtr.docker.vm:2376
     export DOCKER_CERT_PATH=$DIR/dtr
