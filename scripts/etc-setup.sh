1e47c568bbc59ac603ef261c5fe3c451296fe328
Chad Metcalf <metcalfc@gmail.com>
2016-01-25 15:59:49 -0800

    Adding an OU so the CA sticks out better as self signed.

--- scripts/etc-setup.sh
+++ scripts/etc-setup.sh
@@ -38,7 +38,7 @@ metcalfc/certified:latest \
 --root-crl-url=https://ca.enterprise.docker.vm:666/rootca.crl  \
 --crl-url=https://ca.enterprise.docker.vm:666/ca.crl \
 C="US" ST="CA" L="San Francisco" \
-O="Docker" CN="Docker CA"
+O="Docker" OU="Solutions Engineering" CN="Docker CA"
 
 docker run -it --rm -v $(pwd)/etc:/certified/etc \
 -v ~/.gitconfig:/root/.gitconfig \
