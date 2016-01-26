bad7eb3bee8e1b1da646d5a908b43b1e0509bbf9
Chad Metcalf <metcalfc@gmail.com>
2016-01-25 16:00:40 -0800

    Check that we're in the right directory.
    When we docker-compose up we need to be in the right place.

--- scripts/install-dtr.sh
+++ scripts/install-dtr.sh
@@ -36,6 +36,6 @@ if [[ $(docker ps  | grep 'dockerhubenterprise/admin-server' -c) -ne 1 ]]; then
 fi
 
 if [[ $(docker ps  | grep 'notary' -c) -ne 3 ]]; then
-    cd /vagrant/files || echo "Couldn't cd to /vagrant/files" && exit 1
+    cd /vagrant/files || echo "Couldn't cd to /vagrant/files"
     docker-compose up -d
 fi
